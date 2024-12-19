53a5599635150b4191463ab2c98acfe20d167bef
xdavid1999 <xdavidheredia99@gmail.com>
2024-12-19 12:57:36 +0100

    hazardous migration corrected

--- backend/dispatcher/migrations/0002_demo_services.py
+++ backend/dispatcher/migrations/0002_demo_services.py
@@ -1,33 +1,38 @@
 from django.db import migrations
-from dispatcher.models import Topic, Notification
 
 
 def migrate_data(apps, schema_editor):
+    Topic = apps.get_model("dispatcher", "Topic")
+    Notification = apps.get_model("dispatcher", "Notification")
+
+    # Create Demo Sales Topic and Notification
     sales = Topic.objects.create(
         name="Demo sales", description="This is a demo sales service"
     )
     sales_notification = Notification.objects.create(
-        method=Notification.EMAIL,
+        method="Email",
         config={"recipient_list": ["test@test.test", "test2@test.test"]},
     )
     sales.notification = sales_notification
     sales.save()
 
+    # Create Demo Pricing Topic and Notification
     pricing = Topic.objects.create(
         name="Demo pricing", description="This is a demo pricing service"
     )
     pricing_notification = Notification.objects.create(
-        method=Notification.SLACK,
+        method="Slack",
         config={"channel": "#pricing"},
     )
     pricing.notification = pricing_notification
     pricing.save()
 
+    # Create Demo HR Topic and Notification
     human_resources = Topic.objects.create(
         name="Demo HR", description="This is a demo HR service"
     )
     hr_notification = Notification.objects.create(
-        method=Notification.TELEGRAM,
+        method="Telegram",
         config={"chat_id": "123456789"},
     )
     human_resources.notification = hr_notification
@@ -35,6 +40,8 @@ def migrate_data(apps, schema_editor):
 
 
 def reverse_data(apps, schema_editor):
+    Topic = apps.get_model("dispatcher", "Topic")
+
     topics = Topic.objects.filter(name__in=["Demo sales", "Demo pricing", "Demo HR"])
     for topic in topics:
         if topic.notification:
